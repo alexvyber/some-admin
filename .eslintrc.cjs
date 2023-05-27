757798b745499d91887e7b86362f7e0466a8f4ac
Alexey Sokolov <alexvyber@gmail.com>
2023-05-27 14:05:53 +0300

    configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,31 @@
-export default {
-  extends: "next/core-web-vitals",
+module.exports = {
+  $schema: "https://json.schemastore.org/eslintrc",
+  ignorePatterns: ["**/__generated__/*"],
+  root: true,
+  extends: [
+    "next/core-web-vitals",
+    // "turbo",
+    "prettier",
+    "plugin:tailwindcss/recommended",
+    "@alexvyber/eslint-config/react",
+  ],
+  plugins: ["tailwindcss"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    // "react/jsx-key": "off",
+    "tailwindcss/no-custom-classname": "off",
+    "tailwindcss/classnames-order": "off",
+  },
+  settings: {
+    tailwindcss: {
+      callees: ["cn", "cx"],
+      config: "tailwind.config.cjs",
+    },
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
 }
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,17 +1,22 @@
 /** @type {import('prettier').Config} */
 module.exports = {
+  // ...require("@alexvyber/prettier-config"),
   endOfLine: "lf",
+  printWidth: 100,
   semi: false,
   singleQuote: false,
+  arrowParens: "always",
+  useTabs: false,
   tabWidth: 2,
   trailingComma: "es5",
+
   importOrder: [
     "^(react/(.*)$)|^(react$)",
     "^(next/(.*)$)|^(next$)",
     "<THIRD_PARTY_MODULES>",
     "",
+    "",
     "^types$",
-    "^@/env(.*)$",
     "^@/types/(.*)$",
     "^@/config/(.*)$",
     "^@/lib/(.*)$",
@@ -21,13 +26,22 @@ module.exports = {
     "^@/styles/(.*)$",
     "^@/app/(.*)$",
     "",
+    "",
+    "^@/graphql/(.*)$",
+    "",
+    "^@/assets/(.*)$",
+    "",
     "^[./]",
   ],
+
   importOrderSeparation: false,
   importOrderSortSpecifiers: true,
   importOrderBuiltinModulesToTop: true,
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
   importOrderMergeDuplicateImports: true,
   importOrderCombineTypeAndValueImports: true,
-  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss", // MUST come last
+  ],
 }
